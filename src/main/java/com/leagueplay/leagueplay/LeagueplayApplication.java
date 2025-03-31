c02128862b193582c0f51fb1f7b657966cb30f46
Jorge <calonso.jorge@outlook.es>
2025-03-31 22:00:10 +0200

    Primer commit

---
+++ src/main/java/com/leagueplay/leagueplay/LeagueplayApplication.java
@@ -0,0 +1,13 @@
+package com.leagueplay.leagueplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leagueplay/leagueplay/LeagueplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leagueplay.leagueplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
